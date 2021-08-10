7e340b8bed64049009b04f850945b6d3e4cf80d6
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-08-10 17:02:25 -0400

    segundo cambio de url para conectarce al php de github

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ boton.addEventListener("click", (e) =>{
     e.defaultPrevented;
     const conectarphp = async() =>{
         try {
-            const respuesta = await fetch("brayan-bravo-arebalo.github.io/backend/conect.php")
+            const respuesta = await fetch("https://brayan-bravo-arebalo.github.io/backend/conect.php")
             const datos = await respuesta.json()
             console.log(datos);
         } catch (error) {
